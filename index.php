540d9b648b6f0e760e4f1ef16faa4fd6c316ebb9
tina <tostrander@greenriver.edu>
2021-05-18 12:34:21 -0400

    Index page and Pet class created

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+//Turn on error-reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Require the file that defines the pet class
+require('pet.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+    echo '<h1>Hello, pets!</h1>';
+
+    //Instantiate a pet object
+    $pet1 = new Pet();
+
+    //Invoke the pet methods
+    $pet1->eat();
+    $pet1->talk();
+    $pet1->sleep();
+
+
+
+});
+
+//Run fat free
+$f3->run();
---
+++ pet.php
@@ -0,0 +1,24 @@
+<?php
+
+//pet.php
+
+class Pet
+{
+    private $_name;
+    private $_color;
+
+    function eat()
+    {
+        echo "Pet is eating<br>";
+    }
+
+    function talk()
+    {
+        echo "Pet is talking<br>";
+    }
+
+    function sleep()
+    {
+        echo "Pet is sleeping<br>";
+    }
+}
